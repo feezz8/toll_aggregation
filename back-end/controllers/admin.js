6978fac5252a9a93c483a0e4860b7d6447d53273
ntua-el18076 <el18076@mail.ntua.gr>
2025-02-10 17:18:59 +0200

    Add files via upload

--- back-end/controllers/admin.js
+++ back-end/controllers/admin.js
@@ -1,13 +1,15 @@
 const { pool } = require('../utils/database');
 const { fs } = require('fs');
-const { path } = require('path');
-const { csv } = require('csv-parser');
+const path = require('node:path');
 const multer = require('multer');
+const csv = require('fast-csv');
+//const csvParser = require('csv-parser');
 
 
+//const filepath = '/home/konstantinos/Projects/back-end/utils/tollstations2024.csv';
 const upload = multer({ storage: multer.memoryStorage() });
 
-exports.healthcheck = async(res) => {
+exports.healthcheck = async(req, res) => {
     const query = `
     SELECT 
         COUNT(DISTINCT s.ID) as station_count,
@@ -38,41 +40,65 @@ exports.healthcheck = async(res) => {
     });
 };
 
-exports.reset_stations = async(req, res) => {
-    const tollstations2024 = path.dirname('../utils/tollstations2024.csv');
-    const stations = [];
+exports.resetstations = [   
+    upload.single('file'), 
+    async(req, res) => {
+    if(!req.file){
+        return res.status(400).json({error: 'no file '});
+    }
+    if(req.file.mimetype !== 'text/csv'){
+        console.log('Received file:', req.file.originalname);
+        console.log('filetype:', req.file.mimetype);
+        return res.status(400).json({error: 'wrong format'});
+    }
 
     pool.getConnection((err, connection) => {
+        if(err) return res.status(500).json({ status: 'failed', info: 'Connection pool is saturated' });
 
-        if(err) return res.status(500).json({ message: 'Connection pool is saturated' });
-
-        fs.create_read_stream(tollstations2024)
-        .pipe(csv())
-        .on('data', (row) => {
-            stations.push([row.OpId, row.Operator, row.TollID,
-                        row.Name, row.PM, row.Locality, row.Road, row.Lat, 
-                        row.Long, row.Email, row.Price1, row.Price2, row.Price3]);
-        })
-        .on('end', async() => {
-            if(stations.length > 0){
-                const insert_query = `
-                TRUNCATE TABLE toll_stations
-                INSERT INTO toll_stations (OpId, Operator, TollID,
-                        Name, PM, Locality, Road, Lat, 
-                        Long, Email, Price1, Price2, Price3) VALUES ?
-                `;
-                connection(insert_query, [stations], (err) => {
-                    if(err) return res.status(401).json({status: 'failed', info: 'DB connection refused'});
-
-                    connection.release();
-                    return res.status(200).json({status: OK});
-                });
+        trunc_query =`
+        TRUNCATE TABLE toll_stations;
+        `;
+        connection.query(trunc_query, (err) => {
+            if (err) {
+                console.error("Error truncating table:", err);
+                return res.status(401);
             }
+            console.log("Table truncated successfully.");
         });
-    });
-};
+        
+        const csvParser = csv.parse({headers: true});
 
-exports.reset_passes = async(req, res) => {
+        const stations = [];
+        const stream = require('stream');
+        const readable = new stream.Readable();
+        readable._read = () => {}; //No-op
+        readable.push(req.file.buffer);
+        readable.push(null);
+
+        readable.pipe(csvParser)
+            .on('data', (row) => {
+                stations.push([row.OpID, row.Operator, row.TollID,
+                    row.Name, row.PM, row.Locality, row.Road, row.Lat, 
+                    row.Long, row.Email, row.Price1, row.Price2, row.Price3]);
+            })
+            .on('end', async () => {
+                if (stations.length > 0) {
+                    const insert_query = `
+                    INSERT INTO toll_stations (OpID, TollOperator, TollID, TollName, PM, Locality, Road, Latitude, Longtitude, Email, Price1, Price2, Price3) VALUES ?
+                    `;
+                    connection.query(insert_query, [stations], (err) => {
+                        console.log(err);
+                        if(err) return res.status(401).json({status: 'failed', info: 'DB connection refused'});
+
+                        connection.release();
+                        return res.status(200).json({status: 'OK'});
+                    });
+                }
+            });  
+    });
+}];
+
+exports.resetpasses = async(req, res) => {
     const query = `
     TRUNCATE TABLE passes
     `;
@@ -88,16 +114,24 @@ exports.reset_passes = async(req, res) => {
     });
 };
 
-exports.add_passes = [
+exports.addpasses = [
     upload.single('file'), 
     async(req, res) => {
-    if(!req.file || req.file.mimetype !== 'text/csv'){
-        return res.status(400).json({error: 'Invalid file format'});
+    if(!req.file){
+        return res.status(400).json({error: 'no file '});
+    }
+    if(req.file.mimetype !== 'text/csv'){
+        console.log('Received file:', req.file.originalname);
+        console.log('filetype:', req.file.mimetype);
+        return res.status(400).json({error: 'wrong format'});
     }
 
     pool.getConnection((err, connection) => {
         if(err) return res.status(500).json({ status: 'failed', info: 'Connection pool is saturated' });
         
+
+        const csvParser = csv.parse({headers: true});
+
         const passes = [];
         const stream = require('stream');
         const readable = new stream.Readable();
@@ -105,22 +139,21 @@ exports.add_passes = [
         readable.push(req.file.buffer);
         readable.push(null);
 
-        readable    
-            .pipe(csv())
+        readable.pipe(csvParser)
             .on('data', (row) => {
                 passes.push([row.timestamp, row.tollID, row.tagRef, row.tagHomeID, row.charge]);
             })
             .on('end', async () => {
                 if (passes.length > 0) {
                     const query = `
-                    --TRUNCATE TABLE passes
                     INSERT INTO passes (timestamp, tollID, tagRef, tagHomeID, charge) VALUES ?
                     `;
-                    connection(query, [stations], (err) => {
+                    connection.query(query, [passes], (err) => {
+                        console.log(err);
                         if(err) return res.status(401).json({status: 'failed', info: 'DB connection refused'});
     
                         connection.release();
-                        return res.status(200).json({status: OK});
+                        return res.status(200).json({status: 'OK'});
                     });
                 }
             });  
